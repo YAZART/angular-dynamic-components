01128bb2625defbbf7ec9b2573e5890f0cbd74bd
Unknown <design@yazart.ru>
2020-04-15 13:02:50 +0300

    add new method add dynamic component

--- src/app/banner-render/banner-render.component.ts
+++ src/app/banner-render/banner-render.component.ts
@@ -1,70 +1,73 @@
-import { 
-    Component, 
-    OnInit, 
-    ViewChild, 
-    ElementRef, 
-    ɵrenderComponent as renderComponent,
-    ɵLifecycleHooksFeature as LifecycleHooksFeature,
-    Injector, ComponentFactoryResolver, ViewContainerRef } from '@angular/core';
-import { BannerDefaultComponent } from '../banner-types/banner-default/banner-default.component';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  ɵrenderComponent as renderComponent,
+  ɵLifecycleHooksFeature as LifecycleHooksFeature,
+  Injector,
+  ComponentFactoryResolver,
+  ViewContainerRef,
+  ɵrenderComponent,
+} from "@angular/core";
+import { BannerDefaultComponent } from "../banner-types/banner-default/banner-default.component";
 
 @Component({
-    selector: 'app-banner-render',
-    templateUrl: 'banner-render.component.html'
+  selector: "app-banner-render",
+  templateUrl: "banner-render.component.html",
 })
-
 export class BannerRenderComponent implements OnInit {
-    @ViewChild('container', { read: ViewContainerRef, static: true }) container: ViewContainerRef;
+  @ViewChild("container", { read: ViewContainerRef, static: true })
+  container: ViewContainerRef;
+  @ViewChild("container1", { read: ViewContainerRef, static: true })
+  container1: ViewContainerRef;
+  constructor(
+    private resolver: ComponentFactoryResolver,
+    private injector: Injector
+  ) {}
 
-    constructor(
-        private injector: Injector,
-        private resolver: ComponentFactoryResolver
-    ) { }
+  public async add(moduleName: string) {
+    const module = await import(
+      `../banner-types/${moduleName}/${moduleName}.component`
+    );
+    const factory = this.resolver.resolveComponentFactory(
+      module[Object.keys(module)[0]]
+    );
+    const componentRef = this.container1.createComponent(factory);
+  }
 
-    public addNews() {
-        import(`../banner-types/banner-news/banner-news.component`).then(({BannerNewsComponent}) => {
-            const componentFactory = this.resolver.resolveComponentFactory(BannerNewsComponent);
-            const componentRef = this.container.createComponent(componentFactory, 0, this.injector);
-            // return renderComponent(BannerDefaultComponent, { injector, });
-          });
-    }
-    public addSpecial() {   
-        import(`../banner-types/banner-special/banner-special.component`).then(({BannerSpecialComponent}) => {
-            const componentFactory = this.resolver.resolveComponentFactory(BannerSpecialComponent);
-            const componentRef = this.container.createComponent(componentFactory, 0, this.injector);
-            // return renderComponent(BannerDefaultComponent, { injector, });
-          });  
-    }
-    public addMedia() {
-        import(`../banner-types/banner-media/banner-media.component`).then(({BannerMediaComponent}) => {
-            const componentFactory = this.resolver.resolveComponentFactory(BannerMediaComponent);
-            const componentRef = this.container.createComponent(componentFactory, 0, this.injector);
-            // return renderComponent(BannerDefaultComponent, { injector, });
-          }); 
-    }
-    public addTwoCol() {
-        import(`../banner-types/banner-two-col/banner-two-col.component`).then(({BannerTwoColComponent}) => {
-            const componentFactory = this.resolver.resolveComponentFactory(BannerTwoColComponent);
-            const componentRef = this.container.createComponent(componentFactory, 0, this.injector);
-            // return renderComponent(BannerDefaultComponent, { injector, });
-          }); 
-    }
-    addFullBg() {
-        import(`../banner-types/banner-full-bg/banner-full-bg.component`).then(({BannerFullBgComponent}) => {
-            const componentFactory = this.resolver.resolveComponentFactory(BannerFullBgComponent);
-            const componentRef = this.container.createComponent(componentFactory, 0, this.injector);
-            // return renderComponent(BannerDefaultComponent, { injector, });
-        }); 
-    }
-    addDefault() {
-        import(`../banner-types/banner-default/banner-default.component`).then(({BannerDefaultComponent}) => {
-            const componentFactory = this.resolver.resolveComponentFactory(BannerDefaultComponent);
-            const componentRef = this.container.createComponent(componentFactory, 0, this.injector);
-            // return renderComponent(BannerDefaultComponent, { injector, });
-        }); 
-    }
+  public addWithRender() {
+    import("../banner-types/banner-news/banner-news.component").then(
+      ({ BannerNewsComponent }) => {
+        const host = document.createElement('div');
+        this.container.element.nativeElement.appendChild(host);
+        ɵrenderComponent(BannerNewsComponent, {
+          host: host,
+          injector: this.injector,
+        });
+      }
+    );
+  }
 
-    ngOnInit() { }
-    ngOnDestroy() {
-    }
+  public addNews() {
+    this.addWithRender();
+  }
+  public addSpecial() {
+    this.add("banner-special");
+  }
+  public addMedia() {
+    this.add("banner-media");
+  }
+  public addTwoCol() {
+    this.add("banner-two-col");
+  }
+  addFullBg() {
+    this.add("banner-full-bg");
+  }
+  addDefault() {
+    this.add("banner-default");
+  }
+
+  ngOnInit() {}
+  ngOnDestroy() {}
 }
